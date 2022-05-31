9ffdfbf603ef03b0088d3f9681152bb8b728fb11
Matthias Geisler <matthias.geisler@data4life.care>
2022-05-31 07:18:10 +0200

    Add IntArray

---
+++ fixture/src/androidTest/kotlin/tech/antibytes/kfixture/PlatformClassNames.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2022 Matthias Geisler (bitPogo) / All rights reserved.
+ *
+ * Use of this source code is governed by Apache v2.0
+ */
+
+package tech.antibytes.kfixture
+
+actual val boolean: String = "boolean"
+actual val short: String = "short"
+actual val int: String = "int"
+actual val char: String = "char"
+actual val float: String = "float"
+actual val long: String = "long"
+actual val double: String = "double"
+actual val string: String = "java.lang.String"
+actual val uShort: String = "kotlin.UShort"
+actual val uInt: String = "kotlin.UInt"
+actual val uLong: String = "kotlin.ULong"
+actual val byteArray: String = "[B"
+actual val uByteArray: String = "kotlin.UByteArray"
+actual val any: String = "java.lang.Object"
+actual val unit: String = "kotlin.Unit"
---
+++ fixture/src/commonMain/kotlin/tech/antibytes/kfixture/generator/array/IntArrayGenerator.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2022 Matthias Geisler (bitPogo) / All rights reserved.
+ *
+ * Use of this source code is governed by Apache v2.0
+ */
+
+package tech.antibytes.kfixture.generator.array
+
+import co.touchlab.stately.isolate.IsolateState
+import tech.antibytes.kfixture.PublicApi
+import kotlin.random.Random
+
+internal class IntArrayGenerator(
+    val random: IsolateState<Random>
+) : PublicApi.Generator<IntArray> {
+    private fun generateIntArray(size: Int): IntArray {
+        val raw = random.access { it.nextBytes(size) }
+        val fixture = IntArray(size)
+
+        repeat(size) { idx ->
+            fixture[idx] = raw[idx].toInt()
+        }
+
+        return fixture
+    }
+
+    override fun generate(): IntArray {
+        val size = random.access { it.nextInt(1, 100) }
+        
+        return generateIntArray(size)
+    }
+}
