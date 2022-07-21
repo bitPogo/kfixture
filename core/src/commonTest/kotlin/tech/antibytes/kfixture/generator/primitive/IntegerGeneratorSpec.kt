602ba157ed9a41d0e59a60cec4a47e54dfb88057
Matthias Geisler <matthias.geisler@data4life.care>
2022-07-21 03:09:40 +0200

    Add Ranged & SignedNumberGenerator for Integer

--- core/src/commonTest/kotlin/tech/antibytes/kfixture/generator/primitive/IntegerGeneratorSpec.kt
+++ core/src/commonTest/kotlin/tech/antibytes/kfixture/generator/primitive/IntegerGeneratorSpec.kt
@@ -10,6 +10,8 @@ import kotlin.js.JsName
 import kotlin.test.AfterTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertSame
 import kotlin.test.assertTrue
 import tech.antibytes.kfixture.PublicApi
 import tech.antibytes.kfixture.mock.RandomStub
@@ -25,16 +27,16 @@ class IntegerGeneratorSpec {
     @Test
     @Suppress("UNCHECKED_CAST")
     @JsName("fn0")
-    fun `It fulfils Generator`() {
+    fun `It fulfils SignedNumberGenerator`() {
         val generator: Any = IntegerGenerator(random)
 
-        assertTrue(generator is PublicApi.Generator<*>)
+        assertTrue(generator is PublicApi.SignedNumberGenerator<*>)
     }
 
     @Test
     @Suppress("UNCHECKED_CAST")
     @JsName("fn1")
-    fun `Given generate is called it returns a Integer`() {
+    fun `Given generate is called it returns a Int`() {
         // Given
         val expected = 23
         random.nextInt = { expected }
@@ -48,6 +50,150 @@ class IntegerGeneratorSpec {
         assertEquals(
             actual = result,
             expected = expected,
+        )
+    }
+
+    @Test
+    @Suppress("UNCHECKED_CAST")
+    @JsName("fn2")
+    fun `Given generate is called with a range it fails since the start is less equal to the end`() {
+        // Given
+        val expected = IllegalArgumentException()
+
+        random.nextIntRanged = { _, _ -> throw expected }
+
+        val generator = IntegerGenerator(random)
+
+        // Then
+        val error = assertFailsWith<IllegalArgumentException> {
+            // When
+            generator.generate(-1, 0)
+        }
+
+        // Then
+        assertSame(
+            actual = error,
+            expected = expected,
+        )
+    }
+
+    @Test
+    @Suppress("UNCHECKED_CAST")
+    @JsName("fn3")
+    fun `Given generate is called with boundaries it returns a Int`() {
+        // Given
+        val expected = 107
+        val expectedMin = 0
+        val expectedMax = 42
+
+        var capturedMin: Int? = null
+        var capturedMax: Int? = null
+
+        random.nextIntRanged = { givenMin, givenMax ->
+            capturedMin = givenMin
+            capturedMax = givenMax
+
+            expected
+        }
+
+        val generator = IntegerGenerator(random)
+
+        // When
+        val result = generator.generate(
+            from = expectedMin,
+            to = expectedMax,
+        )
+
+        // Then
+        assertEquals(
+            actual = result,
+            expected = expected,
+        )
+        assertEquals(
+            actual = capturedMin,
+            expected = expectedMin,
+        )
+        assertEquals(
+            actual = capturedMax,
+            expected = expectedMax + 1,
+        )
+    }
+
+    @Test
+    @Suppress("UNCHECKED_CAST")
+    @JsName("fn4")
+    fun `Given generate is called with POSITIVE it returns a Int`() {
+        // Given
+        val expected = 107
+
+        var capturedMin: Int? = null
+        var capturedMax: Int? = null
+
+        random.nextIntRanged = { givenMin, givenMax ->
+            capturedMin = givenMin
+            capturedMax = givenMax
+
+            expected
+        }
+
+        val generator = IntegerGenerator(random)
+
+        // When
+        val result = generator.generate(
+            PublicApi.Sign.POSITIVE,
+        )
+
+        // Then
+        assertEquals(
+            actual = result,
+            expected = expected + 1,
+        )
+        assertEquals(
+            actual = capturedMin,
+            expected = -1,
+        )
+        assertEquals(
+            actual = capturedMax,
+            expected = Int.MAX_VALUE,
+        )
+    }
+
+    @Test
+    @Suppress("UNCHECKED_CAST")
+    @JsName("fn5")
+    fun `Given generate is called with NEGATIVE it returns a Int`() {
+        // Given
+        val expected = 107
+
+        var capturedMin: Int? = null
+        var capturedMax: Int? = null
+
+        random.nextIntRanged = { givenMin, givenMax ->
+            capturedMin = givenMin
+            capturedMax = givenMax
+
+            expected
+        }
+
+        val generator = IntegerGenerator(random)
+
+        // When
+        val result = generator.generate(
+            PublicApi.Sign.NEGATIVE,
+        )
+
+        // Then
+        assertEquals(
+            actual = result,
+            expected = expected,
+        )
+        assertEquals(
+            actual = capturedMin,
+            expected = Int.MIN_VALUE,
+        )
+        assertEquals(
+            actual = capturedMax,
+            expected = 1,
         )
     }
 }
