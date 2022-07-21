6d186c0f091800dca6924c4c183778a0ee1de265
Matthias Geisler <matthias.geisler@data4life.care>
2022-07-21 03:44:21 +0200

    Add Ranged & SignedNumberGenerator for Long

--- core/src/commonMain/kotlin/tech/antibytes/kfixture/generator/primitive/LongGenerator.kt
+++ core/src/commonMain/kotlin/tech/antibytes/kfixture/generator/primitive/LongGenerator.kt
@@ -8,9 +8,29 @@ package tech.antibytes.kfixture.generator.primitive
 
 import kotlin.random.Random
 import tech.antibytes.kfixture.PublicApi
+import kotlin.random.nextLong
 
 internal class LongGenerator(
     private val random: Random,
-) : PublicApi.Generator<Long> {
+) : PublicApi.SignedNumberGenerator<Long> {
     override fun generate(): Long = random.nextLong()
+
+    override fun generate(from: Long, to: Long): Long = random.nextLong(LongRange(from, to))
+
+    private fun resolveBoundary(sign: PublicApi.Sign): Pair<Long, Long> {
+        return if (sign == PublicApi.Sign.POSITIVE) {
+            ZERO to Long.MAX_VALUE
+        } else {
+            Long.MIN_VALUE to ZERO
+        }
+    }
+
+    override fun generate(sign: PublicApi.Sign): Long {
+        val (from, to) = resolveBoundary(sign)
+        return generate(from, to)
+    }
+
+    private companion object {
+        const val ZERO = 0L
+    }
 }
--- core/src/commonTest/kotlin/tech/antibytes/kfixture/generator/primitive/LongGeneratorSpec.kt
+++ core/src/commonTest/kotlin/tech/antibytes/kfixture/generator/primitive/LongGeneratorSpec.kt
@@ -10,6 +10,8 @@ import kotlin.js.JsName
 import kotlin.test.AfterTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertSame
 import kotlin.test.assertTrue
 import tech.antibytes.kfixture.PublicApi
 import tech.antibytes.kfixture.mock.RandomStub
@@ -25,10 +27,10 @@ class LongGeneratorSpec {
     @Test
     @Suppress("UNCHECKED_CAST")
     @JsName("fn0")
-    fun `It fulfils Generator`() {
+    fun `It fulfils SignedNumberGenerator`() {
         val generator: Any = LongGenerator(random)
 
-        assertTrue(generator is PublicApi.Generator<*>)
+        assertTrue(generator is PublicApi.SignedNumberGenerator<*>)
     }
 
     @Test
@@ -36,7 +38,7 @@ class LongGeneratorSpec {
     @JsName("fn1")
     fun `Given generate is called it returns a Long`() {
         // Given
-        val expected: Long = 23
+        val expected = 23L
         random.nextLong = { expected }
 
         val generator = LongGenerator(random)
@@ -48,6 +50,150 @@ class LongGeneratorSpec {
         assertEquals(
             actual = result,
             expected = expected,
+        )
+    }
+
+    @Test
+    @Suppress("UNCHECKED_CAST")
+    @JsName("fn2")
+    fun `Given generate is called with a range it fails since the start is less equal to the end`() {
+        // Given
+        val expected = IllegalArgumentException()
+
+        random.nextLongRanged = { _, _ -> throw expected }
+
+        val generator = LongGenerator(random)
+
+        // Then
+        val error = assertFailsWith<IllegalArgumentException> {
+            // When
+            generator.generate(-1, 0)
+        }
+
+        // Then
+        assertSame(
+            actual = error,
+            expected = expected,
+        )
+    }
+
+    @Test
+    @Suppress("UNCHECKED_CAST")
+    @JsName("fn3")
+    fun `Given generate is called with boundaries it returns a Long`() {
+        // Given
+        val expected = 107L
+        val expectedMin = 0L
+        val expectedMax = 42L
+
+        var capturedMin: Long? = null
+        var capturedMax: Long? = null
+
+        random.nextLongRanged = { givenMin, givenMax ->
+            capturedMin = givenMin
+            capturedMax = givenMax
+
+            expected
+        }
+
+        val generator = LongGenerator(random)
+
+        // When
+        val result = generator.generate(
+            from = expectedMin,
+            to = expectedMax,
+        )
+
+        // Then
+        assertEquals(
+            actual = result,
+            expected = expected,
+        )
+        assertEquals(
+            actual = capturedMin,
+            expected = expectedMin,
+        )
+        assertEquals(
+            actual = capturedMax,
+            expected = expectedMax + 1,
+        )
+    }
+
+    @Test
+    @Suppress("UNCHECKED_CAST")
+    @JsName("fn4")
+    fun `Given generate is called with POSITIVE it returns a Long`() {
+        // Given
+        val expected = 107L
+
+        var capturedMin: Long? = null
+        var capturedMax: Long? = null
+
+        random.nextLongRanged = { givenMin, givenMax ->
+            capturedMin = givenMin
+            capturedMax = givenMax
+
+            expected
+        }
+
+        val generator = LongGenerator(random)
+
+        // When
+        val result = generator.generate(
+            PublicApi.Sign.POSITIVE,
+        )
+
+        // Then
+        assertEquals(
+            actual = result,
+            expected = expected + 1,
+        )
+        assertEquals(
+            actual = capturedMin,
+            expected = -1,
+        )
+        assertEquals(
+            actual = capturedMax,
+            expected = Long.MAX_VALUE,
+        )
+    }
+
+    @Test
+    @Suppress("UNCHECKED_CAST")
+    @JsName("fn5")
+    fun `Given generate is called with NEGATIVE it returns a Long`() {
+        // Given
+        val expected = 107L
+
+        var capturedMin: Long? = null
+        var capturedMax: Long? = null
+
+        random.nextLongRanged = { givenMin, givenMax ->
+            capturedMin = givenMin
+            capturedMax = givenMax
+
+            expected
+        }
+
+        val generator = LongGenerator(random)
+
+        // When
+        val result = generator.generate(
+            PublicApi.Sign.NEGATIVE,
+        )
+
+        // Then
+        assertEquals(
+            actual = result,
+            expected = expected,
+        )
+        assertEquals(
+            actual = capturedMin,
+            expected = Long.MIN_VALUE,
+        )
+        assertEquals(
+            actual = capturedMax,
+            expected = 1,
         )
     }
 }
