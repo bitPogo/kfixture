acd937f4559a11c941ba0d01c49f27b9528163cd
Matthias Geisler <matthias.geisler@data4life.care>
2022-07-25 05:47:28 +0200

    Migrate LongArray and ULongArray to SignedNumericArray and RangedArrayGenerator

--- core/src/commonMain/kotlin/tech/antibytes/kfixture/generator/array/LongArrayGenerator.kt
+++ core/src/commonMain/kotlin/tech/antibytes/kfixture/generator/array/LongArrayGenerator.kt
@@ -7,27 +7,15 @@
 package tech.antibytes.kfixture.generator.array
 
 import kotlin.random.Random
-import tech.antibytes.kfixture.FixtureContract.ARRAY_LOWER_BOUND
-import tech.antibytes.kfixture.FixtureContract.ARRAY_UPPER_BOUND
 import tech.antibytes.kfixture.PublicApi
 
 internal class LongArrayGenerator(
-    private val random: Random,
-) : PublicApi.Generator<LongArray> {
-    private fun generateLongArray(size: Int): LongArray {
-        val raw = random.nextBytes(size)
-        val fixture = LongArray(size)
-
-        repeat(size) { idx ->
-            fixture[idx] = raw[idx].toLong()
-        }
-
-        return fixture
-    }
-
-    override fun generate(): LongArray {
-        val size = random.nextInt(ARRAY_LOWER_BOUND, ARRAY_UPPER_BOUND)
-
-        return generateLongArray(size)
-    }
+    random: Random,
+    shortGenerator: PublicApi.SignedNumberGenerator<Long, Long>,
+) : PublicApi.SignedNumericArrayGenerator<Long, LongArray>,
+    SignedArrayNumberGenerator<Long, LongArray>(random, shortGenerator) {
+    override fun arrayBuilder(
+        size: Int,
+        onEach: (idx: Int) -> Long,
+    ): LongArray = LongArray(size, onEach)
 }
--- core/src/commonTest/kotlin/tech/antibytes/kfixture/generator/array/ULongArrayGeneratorSpec.kt
+++ core/src/commonTest/kotlin/tech/antibytes/kfixture/generator/array/ULongArrayGeneratorSpec.kt
@@ -6,6 +6,7 @@
 
 package tech.antibytes.kfixture.generator.array
 
+import co.touchlab.stately.collections.sharedMutableListOf
 import kotlin.js.JsName
 import kotlin.test.AfterTest
 import kotlin.test.Test
@@ -16,6 +17,7 @@ import kotlinx.atomicfu.atomic
 import kotlinx.atomicfu.update
 import tech.antibytes.kfixture.PublicApi
 import tech.antibytes.kfixture.mock.RandomStub
+import tech.antibytes.kfixture.mock.RangedGeneratorStub
 
 class ULongArrayGeneratorSpec {
     private val random = RandomStub()
@@ -31,7 +33,7 @@ class ULongArrayGeneratorSpec {
     @Suppress("UNCHECKED_CAST")
     @JsName("fn0")
     fun `It fulfils Generator`() {
-        val generator: Any = ULongArrayGenerator(random)
+        val generator: Any = ULongArrayGenerator(random, RangedGeneratorStub())
 
         assertTrue(generator is PublicApi.Generator<*>)
     }
@@ -42,18 +44,19 @@ class ULongArrayGeneratorSpec {
     fun `Given generate is called it returns a ULongArray`() {
         // Given
         val size = 23
-        val expected = ULongArray(size)
-        val generator = ULongArrayGenerator(random)
+        val expectedValue = 23.toULong()
+        val expected = ULongArray(size) { expectedValue }
+        val auxiliaryGenerator = RangedGeneratorStub<ULong, ULong>()
 
+        auxiliaryGenerator.generate = { expectedValue }
         random.nextIntRanged = { from, to ->
             range.update { Pair(from, to) }
             size
         }
 
-        random.nextBytesArray = { arraySize -> ByteArray(arraySize) }
-
         // When
-        val result: ULongArray = generator.generate()
+        val generator = ULongArrayGenerator(random, auxiliaryGenerator)
+        val result = generator.generate()
 
         // Then
         assertEquals(
@@ -64,4 +67,270 @@ class ULongArrayGeneratorSpec {
             expected.contentEquals(result),
         )
     }
+
+    @Test
+    @Suppress("UNCHECKED_CAST")
+    @JsName("fn2")
+    fun `Given generate is called with a size it returns a ULongArray in the given size`() {
+        // Given
+        val size = 12
+        val expectedValue = 23.toULong()
+        val expected = ULongArray(size) { expectedValue }
+        val auxiliaryGenerator = RangedGeneratorStub<ULong, ULong>()
+
+        auxiliaryGenerator.generate = { expectedValue }
+
+        // When
+        val generator = ULongArrayGenerator(random, auxiliaryGenerator)
+        val result = generator.generate(size)
+
+        // Then
+        assertEquals(
+            actual = result.size,
+            expected = size,
+        )
+        assertTrue(
+            expected.contentEquals(result),
+        )
+    }
+
+    @Test
+    @Suppress("UNCHECKED_CAST")
+    @JsName("fn3")
+    fun `Given generate is called with boundaries it returns a ULongArray`() {
+        // Given
+        val size = 3
+        val expectedMin = 0.toULong()
+        val expectedMax = 42.toULong()
+
+        var capturedMin: Int? = null
+        var capturedMax: Int? = null
+
+        val auxiliaryGenerator = RangedGeneratorStub<ULong, ULong>()
+
+        val expected = listOf(
+            23.toULong(),
+            7.toULong(),
+            39.toULong(),
+        )
+        val consumableItem = expected.toSharedMutableList()
+
+        random.nextIntRanged = { from, to ->
+            range.update { Pair(from, to) }
+            size
+        }
+
+        auxiliaryGenerator.generateWithRange = { givenMin, givenMax ->
+            capturedMin = givenMin.toInt()
+            capturedMax = givenMax.toInt()
+
+            consumableItem.removeFirst()
+        }
+
+        // When
+        val generator = ULongArrayGenerator(random, auxiliaryGenerator)
+        val result = generator.generate(from = expectedMin, to = expectedMax)
+
+        // Then
+        assertEquals(
+            actual = Pair(1, 10),
+            expected = range.value,
+        )
+        assertEquals(
+            actual = capturedMin,
+            expected = expectedMin.toInt(),
+        )
+        assertEquals(
+            actual = capturedMax,
+            expected = expectedMax.toInt(),
+        )
+        assertTrue(
+            expected.toULongArray().contentEquals(result),
+        )
+    }
+
+    @Test
+    @Suppress("UNCHECKED_CAST")
+    @JsName("fn4")
+    fun `Given generate is called with boundaries it returns a ULongArray with a given Size`() {
+        // Given
+        val size = 3
+        val expectedMin = 0.toULong()
+        val expectedMax = 42.toULong()
+
+        var capturedMin: Int? = null
+        var capturedMax: Int? = null
+
+        val auxiliaryGenerator = RangedGeneratorStub<ULong, ULong>()
+
+        val expected = listOf(
+            23.toULong(),
+            7.toULong(),
+            39.toULong(),
+        )
+        val consumableItem = expected.toSharedMutableList()
+
+        auxiliaryGenerator.generateWithRange = { givenMin, givenMax ->
+            capturedMin = givenMin.toInt()
+            capturedMax = givenMax.toInt()
+
+            consumableItem.removeFirst()
+        }
+
+        // When
+        val generator = ULongArrayGenerator(random, auxiliaryGenerator)
+        val result = generator.generate(from = expectedMin, to = expectedMax, size = size)
+
+        // Then
+        assertEquals(
+            actual = capturedMin,
+            expected = expectedMin.toInt(),
+        )
+        assertEquals(
+            actual = capturedMax,
+            expected = expectedMax.toInt(),
+        )
+        assertTrue(
+            expected.toULongArray().contentEquals(result),
+        )
+    }
+
+    @Test
+    @Suppress("UNCHECKED_CAST")
+    @JsName("fn5")
+    fun `Given generate is called with ranges it returns a ULongArray`() {
+        // Given
+        val expectedMin1 = 0.toULong()
+        val expectedMax1 = 42.toULong()
+        val expectedMin2 = 3.toULong()
+        val expectedMax2 = 41.toULong()
+
+        val capturedMin: MutableList<Int> = sharedMutableListOf()
+        val capturedMax: MutableList<Int> = sharedMutableListOf()
+
+        val auxiliaryGenerator = RangedGeneratorStub<ULong, ULong>()
+
+        val expected = listOf(
+            23.toULong(),
+            7.toULong(),
+            39.toULong(),
+        )
+        val ranges = sharedMutableListOf(3, 1, 0, 1)
+
+        val consumableItem = expected.toSharedMutableList()
+
+        random.nextIntRanged = { from, to ->
+            range.update { Pair(from, to) }
+            ranges.removeFirst()
+        }
+
+        auxiliaryGenerator.generateWithRange = { givenMin, givenMax ->
+            capturedMin.add(givenMin.toInt())
+            capturedMax.add(givenMax.toInt())
+
+            consumableItem.removeFirst()
+        }
+
+        // When
+        val generator = ULongArrayGenerator(random, auxiliaryGenerator)
+        val result = generator.generate(
+            ULongRange(expectedMin1, expectedMax1),
+            ULongRange(expectedMin2, expectedMax2),
+        )
+
+        // Then
+        assertEquals(
+            actual = range.value,
+            expected = Pair(1, 2),
+        )
+        assertTrue(
+            actual = expectedMin1.toInt() in capturedMin,
+        )
+        assertTrue(
+            actual = expectedMin2.toInt() in capturedMin,
+        )
+        assertTrue(
+            actual = expectedMax1.toInt() in capturedMax,
+        )
+        assertTrue(
+            actual = expectedMax2.toInt() in capturedMax,
+        )
+
+        assertTrue(
+            expected.toULongArray().contentEquals(result),
+        )
+    }
+
+    @Test
+    @Suppress("UNCHECKED_CAST")
+    @JsName("fn6")
+    fun `Given generate is called with ranges it returns a ULongArray with a given Size`() {
+        // Given
+        val expectedSize = 3
+        val expectedMin1 = 0.toULong()
+        val expectedMax1 = 42.toULong()
+        val expectedMin2 = 3.toULong()
+        val expectedMax2 = 41.toULong()
+
+        val capturedMin: MutableList<Int> = sharedMutableListOf()
+        val capturedMax: MutableList<Int> = sharedMutableListOf()
+
+        val auxiliaryGenerator = RangedGeneratorStub<ULong, ULong>()
+
+        val expected = listOf(
+            23.toULong(),
+            7.toULong(),
+            39.toULong(),
+        )
+        val ranges = sharedMutableListOf(1, 0, 1)
+
+        val consumableItem = expected.toSharedMutableList()
+
+        random.nextIntRanged = { from, to ->
+            range.update { Pair(from, to) }
+            ranges.removeFirst()
+        }
+
+        auxiliaryGenerator.generateWithRange = { givenMin, givenMax ->
+            capturedMin.add(givenMin.toInt())
+            capturedMax.add(givenMax.toInt())
+
+            consumableItem.removeFirst()
+        }
+
+        // When
+        val generator = ULongArrayGenerator(random, auxiliaryGenerator)
+        val result = generator.generate(
+            ULongRange(expectedMin1, expectedMax1),
+            ULongRange(expectedMin2, expectedMax2),
+            size = expectedSize,
+        )
+
+        // Then
+        assertEquals(
+            actual = range.value,
+            expected = Pair(1, 2),
+        )
+        assertTrue(
+            actual = expectedMin1.toInt() in capturedMin,
+        )
+        assertTrue(
+            actual = expectedMin2.toInt() in capturedMin,
+        )
+        assertTrue(
+            actual = expectedMax1.toInt() in capturedMax,
+        )
+        assertTrue(
+            actual = expectedMax2.toInt() in capturedMax,
+        )
+
+        assertTrue(
+            expected.toULongArray().contentEquals(result),
+        )
+    }
 }
+
+private data class ULongRange(
+    override val start: ULong,
+    override val endInclusive: ULong,
+) : ClosedRange<ULong>
