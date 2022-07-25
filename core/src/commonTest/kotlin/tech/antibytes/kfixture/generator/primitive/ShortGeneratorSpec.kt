c422085f3e62dbf594e17fe81da1171ecf8459ef
Matthias Geisler <matthias.geisler@data4life.care>
2022-07-25 17:59:58 +0200

    Add Filters to ShortGenerator

--- core/src/commonTest/kotlin/tech/antibytes/kfixture/generator/primitive/ShortGeneratorSpec.kt
+++ core/src/commonTest/kotlin/tech/antibytes/kfixture/generator/primitive/ShortGeneratorSpec.kt
@@ -56,6 +56,27 @@ class ShortGeneratorSpec {
     @Test
     @Suppress("UNCHECKED_CAST")
     @JsName("fn2")
+    fun `Given generate is called with a predicate it returns a Short`() {
+        // Given
+        val expected = 23
+        val shorts = mutableListOf(24, 26, expected)
+        random.nextIntRanged = { _, _ -> shorts.removeFirst() }
+
+        val generator = ShortGenerator(random)
+
+        // When
+        val result = generator.generate { short -> short == expected.toShort() }
+
+        // Then
+        assertEquals(
+            actual = result,
+            expected = expected.toShort(),
+        )
+    }
+
+    @Test
+    @Suppress("UNCHECKED_CAST")
+    @JsName("fn3")
     fun `Given generate is called with a range it fails since the start is less equal to the end`() {
         // Given
         val expected = IllegalArgumentException()
@@ -79,7 +100,31 @@ class ShortGeneratorSpec {
 
     @Test
     @Suppress("UNCHECKED_CAST")
-    @JsName("fn3")
+    @JsName("fn4")
+    fun `Given generate is called with a range and a predicate it fails since the start is less equal to the end`() {
+        // Given
+        val expected = IllegalArgumentException()
+
+        random.nextIntRanged = { _, _ -> throw expected }
+
+        val generator = ShortGenerator(random)
+
+        // Then
+        val error = assertFailsWith<IllegalArgumentException> {
+            // When
+            generator.generate(-1, 0) { false }
+        }
+
+        // Then
+        assertSame(
+            actual = error,
+            expected = expected,
+        )
+    }
+
+    @Test
+    @Suppress("UNCHECKED_CAST")
+    @JsName("fn5")
     fun `Given generate is called with boundaries it returns a Short`() {
         // Given
         val expected = 107
@@ -121,7 +166,50 @@ class ShortGeneratorSpec {
 
     @Test
     @Suppress("UNCHECKED_CAST")
-    @JsName("fn4")
+    @JsName("fn6")
+    fun `Given generate is called with boundaries and a predicate it returns a Short`() {
+        // Given
+        val expected = 107
+        val expectedMin = 0.toShort()
+        val expectedMax = 42.toShort()
+        val shorts = mutableListOf(24, 26, expected)
+
+        var capturedMin: Int? = null
+        var capturedMax: Int? = null
+
+        random.nextIntRanged = { givenMin, givenMax ->
+            capturedMin = givenMin
+            capturedMax = givenMax
+
+            shorts.removeFirst()
+        }
+
+        val generator = ShortGenerator(random)
+
+        // When
+        val result = generator.generate(
+            from = expectedMin,
+            to = expectedMax,
+        ) { short -> short == expected.toShort() }
+
+        // Then
+        assertEquals(
+            actual = result,
+            expected = expected.toShort(),
+        )
+        assertEquals(
+            actual = capturedMin,
+            expected = expectedMin.toInt(),
+        )
+        assertEquals(
+            actual = capturedMax,
+            expected = expectedMax.toInt() + 1,
+        )
+    }
+
+    @Test
+    @Suppress("UNCHECKED_CAST")
+    @JsName("fn7")
     fun `Given generate is called with POSITIVE it returns a Short`() {
         // Given
         val expected = 107
@@ -160,7 +248,47 @@ class ShortGeneratorSpec {
 
     @Test
     @Suppress("UNCHECKED_CAST")
-    @JsName("fn5")
+    @JsName("fn8")
+    fun `Given generate is called with a predicate with POSITIVE it returns a Short`() {
+        // Given
+        val expected = 107
+        val shorts = mutableListOf(24, 26, expected)
+
+        var capturedMin: Int? = null
+        var capturedMax: Int? = null
+
+        random.nextIntRanged = { givenMin, givenMax ->
+            capturedMin = givenMin
+            capturedMax = givenMax
+
+            shorts.removeFirst()
+        }
+
+        val generator = ShortGenerator(random)
+
+        // When
+        val result = generator.generate(
+            PublicApi.Sign.POSITIVE,
+        ) { short -> short == expected.toShort() }
+
+        // Then
+        assertEquals(
+            actual = result,
+            expected = expected.toShort(),
+        )
+        assertEquals(
+            actual = capturedMin,
+            expected = 0,
+        )
+        assertEquals(
+            actual = capturedMax,
+            expected = Short.MAX_VALUE + 1,
+        )
+    }
+
+    @Test
+    @Suppress("UNCHECKED_CAST")
+    @JsName("fn9")
     fun `Given generate is called with NEGATIVE it returns a Short`() {
         // Given
         val expected = 107
@@ -181,6 +309,46 @@ class ShortGeneratorSpec {
         val result = generator.generate(
             PublicApi.Sign.NEGATIVE,
         )
+
+        // Then
+        assertEquals(
+            actual = result,
+            expected = expected.toShort(),
+        )
+        assertEquals(
+            actual = capturedMin,
+            expected = Short.MIN_VALUE.toInt(),
+        )
+        assertEquals(
+            actual = capturedMax,
+            expected = 1,
+        )
+    }
+
+    @Test
+    @Suppress("UNCHECKED_CAST")
+    @JsName("fn10")
+    fun `Given generate is called with NEGATIVE and a predicate it returns a Short`() {
+        // Given
+        val expected = 107
+        val shorts = mutableListOf(24, 26, expected)
+
+        var capturedMin: Int? = null
+        var capturedMax: Int? = null
+
+        random.nextIntRanged = { givenMin, givenMax ->
+            capturedMin = givenMin
+            capturedMax = givenMax
+
+            shorts.removeFirst()
+        }
+
+        val generator = ShortGenerator(random)
+
+        // When
+        val result = generator.generate(
+            PublicApi.Sign.NEGATIVE,
+        ) { short -> short == expected.toShort() }
 
         // Then
         assertEquals(
