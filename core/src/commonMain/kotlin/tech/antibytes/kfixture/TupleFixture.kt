7226a25882db8d19813e5faafc4c4464255ddfd9
Matthias Geisler <matthias.geisler@data4life.care>
2022-07-28 12:14:59 +0200

    Add nested generators for Pair

--- core/src/commonMain/kotlin/tech/antibytes/kfixture/TupleFixture.kt
+++ core/src/commonMain/kotlin/tech/antibytes/kfixture/TupleFixture.kt
@@ -8,6 +8,57 @@ package tech.antibytes.kfixture
 
 import kotlin.jvm.JvmName
 import kotlin.reflect.KClass
+import tech.antibytes.kfixture.PublicApi.Qualifier
+
+/**
+ * Creates a Pair of values for given types.
+ * @param First - the type which is supposed to be created for the first value.
+ * @param Second - the type which is supposed to be created for the second value.
+ * @param firstQualifier - a optional qualifier for a special flavour of a type of the first value.
+ * @param firstGenerator a generator (like fixture) which can be delegated for customization.
+ * @param secondQualifier - a optional qualifier for a special flavour of a type of the second value.
+ * @throws IllegalStateException if the no matching Generator was found for the given type.
+ */
+@Throws(IllegalStateException::class)
+public inline fun <reified First, reified Second> PublicApi.Fixture.pairFixture(
+    firstQualifier: Qualifier? = null,
+    firstGenerator: Function1<Qualifier?, First>,
+    secondQualifier: Qualifier? = null,
+): Pair<First, Second> = firstGenerator.invoke(firstQualifier) to fixture(secondQualifier)
+
+/**
+ * Creates a Pair of values for given types.
+ * @param First - the type which is supposed to be created for the first value.
+ * @param Second - the type which is supposed to be created for the second value.
+ * @param firstQualifier - a optional qualifier for a special flavour of a type of the first value.
+ * @param secondQualifier - a optional qualifier for a special flavour of a type of the second value.
+ * @param secondGenerator a generator (like fixture) which can be delegated for customization.
+ * @throws IllegalStateException if the no matching Generator was found for the given type.
+ */
+@Throws(IllegalStateException::class)
+public inline fun <reified First, reified Second> PublicApi.Fixture.pairFixture(
+    firstQualifier: Qualifier? = null,
+    secondQualifier: Qualifier? = null,
+    secondGenerator: Function1<Qualifier?, Second>,
+): Pair<First, Second> = fixture<First>(firstQualifier) to secondGenerator.invoke(secondQualifier)
+
+/**
+ * Creates a Pair of values for given types.
+ * @param First - the type which is supposed to be created for the first value.
+ * @param Second - the type which is supposed to be created for the second value.
+ * @param firstQualifier - a optional qualifier for a special flavour of a type of the first value.
+ * @param firstGenerator a generator (like fixture) which can be delegated for customization.
+ * @param secondQualifier - a optional qualifier for a special flavour of a type of the second value.
+ * @param secondGenerator a generator (like fixture) which can be delegated for customization.
+ * @throws IllegalStateException if the no matching Generator was found for the given type.
+ */
+@Throws(IllegalStateException::class)
+public inline fun <reified First, reified Second> PublicApi.Fixture.pairFixture(
+    firstQualifier: Qualifier? = null,
+    firstGenerator: Function1<Qualifier?, First>,
+    secondQualifier: Qualifier? = null,
+    secondGenerator: Function1<Qualifier?, Second>,
+): Pair<First, Second> = firstGenerator.invoke(firstQualifier) to secondGenerator.invoke(secondQualifier)
 
 /**
  * Creates a Pair of values for given types.
@@ -19,9 +70,87 @@ import kotlin.reflect.KClass
  */
 @Throws(IllegalStateException::class)
 public inline fun <reified First, reified Second> PublicApi.Fixture.pairFixture(
-    firstQualifier: PublicApi.Qualifier? = null,
-    secondQualifier: PublicApi.Qualifier? = null,
+    firstQualifier: Qualifier? = null,
+    secondQualifier: Qualifier? = null,
 ): Pair<First, Second> = fixture<First>(firstQualifier) to fixture(secondQualifier)
+
+@Suppress("UNUSED_PARAMETER")
+@JvmName("pairFixtureAlias")
+/**
+ * Creates a Pair of values for given types.
+ * @param First - the type which is supposed to be created for the first value.
+ * @param Second - the type which is supposed to be created for the second value.
+ * @param C the enclosing Pair.
+ * @param type the identifying type of the generic.
+ * @param firstQualifier - a optional qualifier for a special flavour of a type of the first value.
+ * @param firstGenerator a generator (like fixture) which can be delegated for customization.
+ * @param secondQualifier - a optional qualifier for a special flavour of a type of the second value.
+ * @throws IllegalStateException if the no matching Generator was found for the given type.
+ */
+@Throws(IllegalStateException::class)
+public inline fun <reified C : Pair<First, Second>, reified First, reified Second> PublicApi.Fixture.fixture(
+    type: KClass<Pair<*, *>>,
+    firstQualifier: Qualifier? = null,
+    firstGenerator: Function1<Qualifier?, First>,
+    secondQualifier: Qualifier? = null,
+): C = pairFixture<First, Second>(
+    firstQualifier = firstQualifier,
+    firstGenerator = firstGenerator,
+    secondQualifier = secondQualifier,
+) as C
+
+@Suppress("UNUSED_PARAMETER")
+@JvmName("pairFixtureAlias")
+/**
+ * Creates a Pair of values for given types.
+ * @param First - the type which is supposed to be created for the first value.
+ * @param Second - the type which is supposed to be created for the second value.
+ * @param C the enclosing Pair.
+ * @param type the identifying type of the generic.
+ * @param firstQualifier - a optional qualifier for a special flavour of a type of the first value.
+ * @param secondQualifier - a optional qualifier for a special flavour of a type of the second value.
+ * @param secondGenerator a generator (like fixture) which can be delegated for customization.
+ * @throws IllegalStateException if the no matching Generator was found for the given type.
+ */
+@Throws(IllegalStateException::class)
+public inline fun <reified C : Pair<First, Second>, reified First, reified Second> PublicApi.Fixture.fixture(
+    type: KClass<Pair<*, *>>,
+    firstQualifier: Qualifier? = null,
+    secondQualifier: Qualifier? = null,
+    secondGenerator: Function1<Qualifier?, Second>,
+): C = pairFixture<First, Second>(
+    firstQualifier = firstQualifier,
+    secondQualifier = secondQualifier,
+    secondGenerator = secondGenerator,
+) as C
+
+@Suppress("UNUSED_PARAMETER")
+@JvmName("pairFixtureAlias")
+/**
+ * Creates a Pair of values for given types.
+ * @param First - the type which is supposed to be created for the first value.
+ * @param Second - the type which is supposed to be created for the second value.
+ * @param C the enclosing Pair.
+ * @param type the identifying type of the generic.
+ * @param firstQualifier - a optional qualifier for a special flavour of a type of the first value.
+ * @param firstGenerator a generator (like fixture) which can be delegated for customization.
+ * @param secondQualifier - a optional qualifier for a special flavour of a type of the second value.
+ * @param secondGenerator a generator (like fixture) which can be delegated for customization.
+ * @throws IllegalStateException if the no matching Generator was found for the given type.
+ */
+@Throws(IllegalStateException::class)
+public inline fun <reified C : Pair<First, Second>, reified First, reified Second> PublicApi.Fixture.fixture(
+    type: KClass<Pair<*, *>>,
+    firstQualifier: Qualifier? = null,
+    firstGenerator: Function1<Qualifier?, First>,
+    secondQualifier: Qualifier? = null,
+    secondGenerator: Function1<Qualifier?, Second>,
+): C = pairFixture(
+    firstQualifier = firstQualifier,
+    firstGenerator = firstGenerator,
+    secondQualifier = secondQualifier,
+    secondGenerator = secondGenerator,
+) as C
 
 @Suppress("UNUSED_PARAMETER")
 @JvmName("pairFixtureAlias")
@@ -38,8 +167,8 @@ public inline fun <reified First, reified Second> PublicApi.Fixture.pairFixture(
 @Throws(IllegalStateException::class)
 public inline fun <reified C : Pair<First, Second>, reified First, reified Second> PublicApi.Fixture.fixture(
     type: KClass<Pair<*, *>>,
-    firstQualifier: PublicApi.Qualifier? = null,
-    secondQualifier: PublicApi.Qualifier? = null,
+    firstQualifier: Qualifier? = null,
+    secondQualifier: Qualifier? = null,
 ): C = pairFixture<First, Second>(
     firstQualifier = firstQualifier,
     secondQualifier = secondQualifier,
@@ -57,9 +186,9 @@ public inline fun <reified C : Pair<First, Second>, reified First, reified Secon
  */
 @Throws(IllegalStateException::class)
 public inline fun <reified First, reified Second, reified Third> PublicApi.Fixture.tripleFixture(
-    firstQualifier: PublicApi.Qualifier? = null,
-    secondQualifier: PublicApi.Qualifier? = null,
-    thirdQualifier: PublicApi.Qualifier? = null,
+    firstQualifier: Qualifier? = null,
+    secondQualifier: Qualifier? = null,
+    thirdQualifier: Qualifier? = null,
 ): Triple<First, Second, Third> {
     return Triple(
         fixture(firstQualifier),
@@ -85,9 +214,9 @@ public inline fun <reified First, reified Second, reified Third> PublicApi.Fixtu
 @Throws(IllegalStateException::class)
 public inline fun <reified C : Triple<First, Second, Third>, reified First, reified Second, reified Third> PublicApi.Fixture.fixture(
     type: KClass<Triple<*, *, *>>,
-    firstQualifier: PublicApi.Qualifier? = null,
-    secondQualifier: PublicApi.Qualifier? = null,
-    thirdQualifier: PublicApi.Qualifier? = null,
+    firstQualifier: Qualifier? = null,
+    secondQualifier: Qualifier? = null,
+    thirdQualifier: Qualifier? = null,
 ): C = tripleFixture<First, Second, Third>(
     firstQualifier = firstQualifier,
     secondQualifier = secondQualifier,
